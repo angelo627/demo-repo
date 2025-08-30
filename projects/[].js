8b9598ae5b5c5f46d89f028b219782db19f19e04
angelo627 <angelochineamerem@gmail.com>
2025-08-30 01:58:11 -0800

    aug 30

---
+++ projects/[].js
@@ -0,0 +1,28 @@
+
+        // alert(document.documentElement.getAttribute("lang"));
+        // alert(document.head.firstElementChild.innerHTML);
+        document.body.style.backgroundColor = "green";
+        const body = document.getElementById("gp");
+        body.style.color = "blue"
+        const elements = document.getElementsByTagName("h3");
+        for (let i = 0; i < elements.length; i++){
+            elements[i].style.color = "gray";
+        }
+
+        const tin = document.getElementsByTagName("h4");
+        for (let h = 0; h < tin.length; h++){
+        tin[h].style.color = "red"}
+
+
+        const game = document.getElementsByTagName("h5");
+        for ( let y = 0; y < game.length; y++){
+        game[y].style.color = "purple"}
+
+
+
+        const t = document.getElementsByTagName("hr");
+        for ( let i = 0; i < t.length; i++){
+            t[i].style.backgroundColor = "brown"
+            t[i].style.height = "2px";
+            t[i].style.border = "none"; 
+        }
